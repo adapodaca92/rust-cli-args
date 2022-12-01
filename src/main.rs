ed6eb728954916ef391ffde85ceffa5c9b2817d4
Anthony Apodaca <adapodaca92@gmail.com>
2022-11-30 23:59:21 -0800

    First commit of completed CLI program with functional grep.

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguements: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for lines of text with '{}'...", config.query);
+    println!("in file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+
+
+}
+
+
